01d52a8491ca0ca78516d74c28fc4e4aafaf72bc
levy <15958732798@163.com>
2021-10-12 14:39:28 +0800

    懒汉单例模式线程安全及线程非安全实现

--- creational/lazy_singleton_test.go
+++ creational/lazy_singleton_test.go
@@ -11,3 +11,10 @@ func TestUnsafeLazySingleton(t *testing.T) {
 	s2 := UnsafeLazySingleton()
 	fmt.Println("This is", s2["this"])
 }
+
+func TestSafeLazySingleton(t *testing.T) {
+	s1 := SafeLazySingleton()
+	s1["this"] = "one"
+	s2 := SafeLazySingleton()
+	fmt.Println("This is", s2["this"])
+}
