111c636c4a3d221a30b729f44b4167e63aa8d67b
levy <15958732798@163.com>
2021-10-14 14:43:31 +0800

    抽象工厂模式修改测试

--- creational/abstract_factory.go
+++ creational/abstract_factory.go
@@ -2,36 +2,96 @@ package creational
 
 import "fmt"
 
-type Worker interface {
-	Cal(int, int) (float64, error)
+//键盘接口
+type Keyboard interface {
+	input()
 }
 
-type WorkerCreator interface {
-	Create() Worker
+//鼠标接口
+type Mouse interface {
+	click()
 }
 
-//数学运算产品族
-type ProgrammerOperation struct {
+//Mac产品族
+type MacKeyboard struct {
 }
 
-//数学运算产品族-加法
-func (p *ProgrammerOperation) Cal(num1 int, num2 int) (float64, error) {
-	fmt.Println("result", num1+num2)
-	return float64(num1 + num2), nil
+type MacMouse struct {
 }
 
-//数学运算产品族-减法
-func (p *ProgrammerOperation) Minus(num1 int, num2 int) (float64, error) {
-	fmt.Println("result", num1+num2)
-	return float64(num1 - num2), nil
+//windows产品族
+type WindowsKeyboard struct {
 }
 
-//抽象工厂
-type ProgrammerCreator struct {
+type WindowsMouse struct {
 }
 
-//创建数学运算产品的工厂
-func (pc *ProgrammerCreator) CreateOperation() Worker {
-	s := new(ProgrammerOperation)
-	return s
+func (macKeyboard *MacKeyboard) input() {
+	fmt.Println("Create Mac keyboard")
+}
+
+func (macMouse *MacMouse) click() {
+	fmt.Println("Create Mac mouse")
+}
+
+func (windowsKeyboard *WindowsKeyboard) input() {
+	fmt.Println("Create Windows keyboard")
+}
+
+func (windowsMouse *WindowsMouse) click() {
+	fmt.Println("Create Windows mouse")
+}
+
+//定义工厂接口
+type HardWare interface {
+	CreateKeyboard() Keyboard
+	CreateMouse() Mouse
+}
+
+type FactoryProducer struct {
+}
+
+//Mac工厂，生产Mac产品族
+type MacFactory struct {
+}
+
+//生产产品等级键盘
+func (mf *MacFactory) CreateKeyboard() Keyboard {
+	return new(MacKeyboard)
+}
+
+//生产产品等级鼠标
+func (mf *MacFactory) CreateMouse() Mouse {
+	return new(MacMouse)
+}
+
+//Windows工厂，生产windows产品族
+type WindowsFactory struct {
+}
+
+func (wf *WindowsFactory) CreateKeyboard() Keyboard {
+	return new(WindowsKeyboard)
+}
+
+func (wf *WindowsFactory) CreateMouse() Mouse {
+	return new(WindowsMouse)
+}
+
+func (fp *FactoryProducer) CreateFactory(factoryName string) HardWare {
+	switch factoryName {
+	case "mac":
+		return &MacFactory{}
+	case "windows":
+		return &WindowsFactory{}
+	default:
+		return nil
+	}
+}
+func NewAbstract() {
+	fp := &FactoryProducer{}
+	macFactory := fp.CreateFactory("mac")
+	macKeyboard := macFactory.CreateKeyboard()
+	macMouse := macFactory.CreateMouse()
+	macKeyboard.input()
+	macMouse.click()
 }
--- creational/abstract_factory_test.go
+++ creational/abstract_factory_test.go
@@ -1,1 +1,7 @@
 package creational
+
+import "testing"
+
+func TestNewAbstract(t *testing.T) {
+	NewAbstract()
+}
