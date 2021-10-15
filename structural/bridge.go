78348fa643ba323608a2020f5f895a19d0d6b1d3
levy <15958732798@163.com>
2021-10-15 09:36:13 +0800

    桥接模式完成

--- structural/bridge.go
+++ structural/bridge.go
@@ -10,26 +10,32 @@ type MessageImplementer interface {
 	Send(text, to string)
 }
 
+//SMS消息
 type MessageSMS struct {
 }
 
 func (*MessageSMS) Send(text, to string) {
 	fmt.Printf("send %s to %s via SMS", text, to)
 }
+
+//通往SMS消息的桥梁
 func ViaSMS() MessageImplementer {
 	return &MessageSMS{}
 }
 
+//邮件消息
 type MessageEmail struct{}
 
 func (*MessageEmail) Send(text, to string) {
 	fmt.Printf("send %s to %s via Email", text, to)
 }
 
+//通往邮件消息的桥梁
 func ViaEmail() MessageImplementer {
 	return &MessageEmail{}
 }
 
+//建造桥梁
 type CommonMessage struct {
 	method MessageImplementer
 }
