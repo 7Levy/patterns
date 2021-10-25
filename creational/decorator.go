9520f799f80f30c45d23c5faf88a996480841785
levy <15958732798@163.com>
2021-10-25 23:11:38 +0800

    装饰器模式完成

---
+++ creational/decorator.go
@@ -0,0 +1,22 @@
+package creational
+
+import (
+	"fmt"
+	"time"
+)
+
+func myFunc() {
+	fmt.Println("Hello World")
+	time.Sleep(1 * time.Second)
+}
+
+func coolFunc(a func()) {
+	fmt.Printf("Starting function execution: %s\n", time.Now())
+	a()
+	fmt.Printf("End of function execution: %s\n", time.Now())
+}
+
+func Decorator() {
+	fmt.Printf("Type: %T\n", myFunc)
+	coolFunc(myFunc)
+}
